7832f795e2c934cdd4894544c2144423115441c6
Callum Kloos <callumkloos@gmail.com>
2023-05-03 17:04:13 +0100

    added a help command test

--- main.ts
+++ main.ts
@@ -9,8 +9,7 @@ import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
 
 // import our functions from other files
 import { verifySignature } from "./utils/discordApi.ts";
-
-
+import { InteractionResponseType, InteractionType } from "./types/index.ts";
 
 serve({
 	"/": home,
@@ -18,11 +17,13 @@ serve({
 
 /** handler for our requests from discord */
 async function home(request: Request) {
-    if (request.method === "GET") {
-        return json({ hello: "world" });
-    }
+	// if the request is a GET request, return a simple hello world
+	if (request.method === "GET") {
+		return json({ hello: "world" });
+	}
 
 	// validate the request
+	// Can be extracted to middleware later
 	const { error } = await validateRequest(request, {
 		POST: {
 			headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
@@ -37,23 +38,39 @@ async function home(request: Request) {
 		return json({ error: "Invalid Request" }, { status: 401 });
 	}
 
+	// extract the body of the request (and provide default values)
 	const { type = 0, data = { options: [] } } = JSON.parse(body);
-	if (type === 1) {
-		// ping interaction from discord
+	if (type === InteractionType.PING) {
 		return json({ type: 1 });
 	}
 
-	if (type === 2) {
-        // command interaction from discord
-		const { value } = data.options.find(
-			(option: { name: string }) => option.name === "name",
-		);
-		return json({
-			type: 4,
-			data: {
-				content: `hello ${value}!`,
-			},
-		});
+	if (type === InteractionType.APPLICATION_COMMAND) {
+		// extract name and options from data
+		const { name, options } = data;
+		if (name === "help") {
+			return json({
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					content:
+						"Hey, I am glad you are asking for help, noob. What do you need help with?",
+					components: [
+						{
+							type: 1, // add enum for component types later
+							components: [
+								{
+									type: 2,
+									label: "How to use this bot",
+								},
+								{
+									type: 2,
+									label: "What commands are available",
+								},
+							],
+						},
+					],
+				},
+			});
+		}
 	}
 
 	return json(
