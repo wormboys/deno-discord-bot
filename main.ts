531446d192ad67fa3174b56da63f869f63c1efe4
Callum Kloos <callumkloos@gmail.com>
2023-05-04 16:41:13 +0100

    fixed

--- main.ts
+++ main.ts
@@ -44,46 +44,44 @@ async function home(request: Request) {
 
 	// extract the body of the request (and provide default values)
 	const { type = 0, data = { options: [] } } = JSON.parse(body);
-	try {
-		if (type == InteractionType.ApplicationCommand) {
-			// extract name and options from data
-			const { name } = data;
-			if (name === "help") {
-				console.log("help command triggered");
-				return json({
-					type: InteractionResponseType.ChannelMessageWithSource,
-					data: {
-						content: "you did the thing",
-						components: [
-							{
-								type: 1,
-								components: [
-									{
-										type: 2,
-                                        custom_id: "test",
-										label: "Button",
-										style: 1,
-									},
-									{
-										type: 2,
-                                        custom_id: "test2",
-										label: "Button2",
-										style: 1,
-									},
-								],
-							},
-						],
-					},
-				});
-			}
-		}
-	} catch (error) {
-		console.log(error);
-	}
 	if (type == InteractionType.Ping) {
 		return json({ type: 1 });
 	}
 
+	if (type == InteractionType.ApplicationCommand) {
+		// extract name and options from data
+		const { name } = data;
+		if (name === "help") {
+			console.log("help command triggered");
+			return json({
+				type: InteractionResponseType.ChannelMessageWithSource,
+				data: {
+					content: "you did the thing",
+					components: [
+						{
+							type: 1,
+							components: [
+								{
+									type: 2,
+                                    custom_id: "test",
+									label: "Button",
+									style: 1,
+								},
+								{
+									type: 2,
+                                    custom_id: "test2",
+									label: "Button2",
+									style: 1,
+								},
+							],
+						},
+					],
+				},
+			});
+		}
+	}
+
+	console.log(request);
 	return json(
 		{
 			error: "bad request",
